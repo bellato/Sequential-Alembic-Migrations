7817f96bccc697545016a430cce80e8b9200ba8d
ZubnayaFeya <danilal@mail.ru>
2025-11-05 16:55:48 +0300

    Initial commit

---
+++ models/user_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<User(username={self.username}, email={self.email})>"
